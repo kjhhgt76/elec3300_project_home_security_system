4a8fa3890ba5ee1a777a9aee4917be7305e51393
athena-hui <athenahui2001@gmail.com>
2022-04-14 00:08:10 +0800

    upload touchpad

---
+++ ELEC3300_Touchpad/Inc/own_define_functions.h
@@ -0,0 +1,36 @@
+#ifndef __OWN_DEFINE_FUNCTIONS_H
+#define __OWN_DEFINE_FUNCTIONS_H
+
+#include "fatfs.h"
+#include "lcd_own.h"
+#include "bsp_ov7725.h"
+#include "bsp_sccb.h"
+#include <string.h>
+#include "bmp.h"
+#include "xpt2046.h"
+#include "main.h"
+
+//variables for password module
+extern uint8_t key_read;
+extern char password[4];
+extern char password_enter[4];
+extern int door_status;
+
+//variables for SDIO module
+extern FATFS photoFATFS;
+extern FIL photoFILE;
+extern UINT numberofbytes;
+extern BITMAPINFO bmp;
+extern int photoNum;
+
+//variables for camera module
+extern volatile uint8_t Ov7725_vsync ;
+
+char read_keypad (void);		       //function to read keypad
+void enterPassword(char*);		       //function to set and enter password
+void verifyPassword(const char*, char*, int*, int*); //function to verify password & following actions
+int newbmp(const char*); 		       //function to write new bmp
+void createPhotoPath(int*, char*); 	       //function to create next photo path
+void check_homepage(void);		       //function to check homepage button press
+
+#endif
